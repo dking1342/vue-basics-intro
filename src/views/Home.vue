916c130b313ab6773a5c5439cd9394c557822625
David King <leanmachine42@yahoo.com>
2021-09-05 00:27:19 -0500

    init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Modal />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Modal from "../components/Modal.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Modal,
   },
 };
 </script>
